e3b80306cd697465aa51f42d14ca77d391a0a0ee
Jan Srnicek <jsrnicek@cisco.com>
2016-09-12 13:12:14 +0200

    HONEYCOMB-142 - Subnet validation
    Added validation of address to not be from same
    subnet
    Refactored to detect conflicts with mixed types of subnets and report them better
    
    Change-Id: Ib815a79c9f61e88b5097884e346320028bbfe914
    Signed-off-by: Jan Srnicek <jsrnicek@cisco.com>
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- v3po/v3po2vpp/src/main/java/io/fd/honeycomb/translate/v3po/interfaces/ip/Ipv4Customizer.java
+++ v3po/v3po2vpp/src/main/java/io/fd/honeycomb/translate/v3po/interfaces/ip/Ipv4Customizer.java
@@ -42,37 +42,25 @@ public class Ipv4Customizer extends FutureJVppCustomizer implements WriterCustom
     @Override
     public void writeCurrentAttributes(@Nonnull final InstanceIdentifier<Ipv4> id,
                                        @Nonnull final Ipv4 dataAfter, @Nonnull final WriteContext writeContext)
-        throws WriteFailedException {
-        final String ifcName = id.firstKeyOf(Interface.class).getName();
-        setIpv4(id, ifcName, dataAfter, writeContext);
+            throws WriteFailedException {
+
+        //TODO - add subnet validation after HONEYCOMB-201
     }
 
     @Override
     public void updateCurrentAttributes(@Nonnull final InstanceIdentifier<Ipv4> id,
                                         @Nonnull final Ipv4 dataBefore, @Nonnull final Ipv4 dataAfter,
                                         @Nonnull final WriteContext writeContext)
-        throws WriteFailedException {
+            throws WriteFailedException {
         final String ifcName = id.firstKeyOf(Interface.class).getName();
 
         // TODO handle update in a better way
-        setIpv4(id, ifcName, dataAfter, writeContext);
     }
 
     @Override
     public void deleteCurrentAttributes(@Nonnull final InstanceIdentifier<Ipv4> id,
                                         @Nonnull final Ipv4 dataBefore, @Nonnull final WriteContext writeContext) {
-        // TODO implement delete
-    }
-
-    private void setIpv4(final InstanceIdentifier<Ipv4> id, final String name, final Ipv4 ipv4,
-                         final WriteContext writeContext)
-        throws WriteFailedException {
-        final int swIfc = interfaceContext.getIndex(name, writeContext.getMappingContext());
-
-        LOG.warn("Ignoring Ipv4 leaf nodes (create/update is not supported)");
-        // TODO add support for enabled leaf
-        // TODO add support for forwarding leaf
-        // TODO add support for mtu leaf
+        // TODO HONEYCOMB-180 implement delete
     }
 
 }
