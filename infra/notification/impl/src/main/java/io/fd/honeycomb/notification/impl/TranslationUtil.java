2b3a311ca96e157a05c85128e28ae7251ab9bdd9
Marek Gradzki <mgradzki@cisco.com>
2018-02-02 11:38:06 +0100

    Use XMLStreamNormalizedNodeStreamWriter from yang-data-codec-xml
    instead of old and deprecated version from yang-data-impl.
    
    Change-Id: If3f9dbffe83cdde29d2f4996dbd299a60bab90a9
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- infra/notification/impl/src/main/java/io/fd/honeycomb/notification/impl/TranslationUtil.java
+++ infra/notification/impl/src/main/java/io/fd/honeycomb/notification/impl/TranslationUtil.java
@@ -28,7 +28,7 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
-import org.opendaylight.yangtools.yang.data.impl.codec.xml.XMLStreamNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.codec.xml.XMLStreamNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.slf4j.Logger;
